f123118b171d002bc520e9871931585fa9637a1f
Saurabh-R2003 <saurabhr2003@gmail.com>
2025-04-29 17:33:02 +0530

    port changes

--- app.py
+++ app.py
@@ -157,4 +157,4 @@ def reset_conversation():
 
 if __name__ == '__main__':
     print("Starting Investment Advisor web server...")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
